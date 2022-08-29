1077cbf01fd38fe8c970b4fead9d694b646eade3
Dariusz Okonski <darek200180@gmail.com>
2022-08-29 22:02:39 +0100

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import './App.css';
-
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>        
-
-        
+        <h1>Hello</h1>              
       </header>
     
     </div>
